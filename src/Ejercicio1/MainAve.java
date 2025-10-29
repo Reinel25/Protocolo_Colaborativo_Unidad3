32af1210677125b19e2d79e44f1cf63f866cdbe7
Reinel <rcaballeron@unicartagena.edu.co>
2025-10-29 11:20:00 -0500

    Ejercicio1 completado

---
+++ src/Ejercicio1/MainAve.java
@@ -0,0 +1,13 @@
+package Ejercicio1;
+
+public class MainAve {
+    public static void main(String[] args) {
+
+        Ave ave = new Ave();
+        ave.cantar();
+        ave.volar();
+    }
+}
+
+
+
